ae17cd9581b290c16f127905abe44f4d4fe0f4f4
notlongfen <phannguyenhoanglong@gmail.com>
2025-07-14 20:12:02 +0700

    chore(pinata): add environment and test connection

--- src/utils/configPinata.ts
+++ src/utils/configPinata.ts
@@ -1,8 +1,17 @@
 "server only"
 
 import { PinataSDK } from "pinata"
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log(process.env.PINATA_JWT)
+console.log(process.env.NEXT_PUBLIC_GATEWAY_URL)
 
 export const pinata = new PinataSDK({
   pinataJwt: `${process.env.PINATA_JWT}`,
   pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
-})
+});
+
+pinata.testAuthentication().catch((error: any) => {
+  console.error("Pinata authentication failed:", error);
+});
